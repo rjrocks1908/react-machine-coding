458bd444605f0929edcddc29fd6bddc79e9f7d77
Rahul Jain <rj.rahul1908@gmail.com>
2025-06-03 17:55:41 +0530

    fix: correct formatting and unused variable in OtpPage component

--- src/pages/OtpPage.tsx
+++ src/pages/OtpPage.tsx
@@ -3,13 +3,13 @@ import Box from "../components/otp/box";
 import "../styles/otp.css";
 
 function OtpPage() {
-  const [otp, setOtp] = useState(Array.from({ length: 6 }));
-  const inputRefs = useRef<HTMLInputElement[]>([])
+  const [_, setOtp] = useState(Array.from({ length: 6 }));
+  const inputRefs = useRef<HTMLInputElement[]>([]);
 
   return (
     <div className="container">
       {Array.from({ length: 6 }, (_, i) => (
-        <Box key={i} id={i} setOtp={setOtp} inputRefs={inputRefs}/>
+        <Box key={i} id={i} setOtp={setOtp} inputRefs={inputRefs} />
       ))}
     </div>
   );
